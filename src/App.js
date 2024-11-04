423b766bbcd22825dcd945592905cc2adfad9452
Amaan7985 <160345854+Amaan7985@users.noreply.github.com>
2024-11-03 23:35:53 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to Fullstack Development - I</h1>
+        <p>
+          React JS Programming Week09 Lab exercise
+        </p>
+        <p>101436093</p>
+        <p>Mohammad Amaan Vohra</p>
+        <p>George Brown College, Toronto</p>
+
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
